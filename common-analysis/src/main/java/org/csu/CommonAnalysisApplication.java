690cf2292d68ba66dc738707a9fc89a9e1ed08f8
lixiang <csucoderlee@foxmail.com>
2018-03-09 14:58:39 +0800

    first commit

---
+++ common-analysis/src/main/java/org/csu/CommonAnalysisApplication.java
@@ -0,0 +1,13 @@
+package org.csu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,})
+public class CommonAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonAnalysisApplication.class, args);
+	}
+}
